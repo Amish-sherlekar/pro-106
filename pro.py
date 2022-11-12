5d8038b4e1a58999aa9d3794d7cc809a205167ff
Amish-sherlekar <asmitasherlekar@gmail.com>
2022-11-12 20:59:32 +0530

    first commit

---
+++ pro.py
@@ -0,0 +1,33 @@
+import csv
+import plotly_express as px
+import numpy as np
+
+data = open("Student Marks vs Days Present.csv")
+df = csv.DictReader(data)
+p = px.scatter(df, x="Days Present", y="Marks In Percentage")
+p.show()
+
+def fetch_data_source(data_path):
+    Days_Present = []
+    Percentage = []
+
+    with open(data_path) as f:
+        data = csv.DictReader(f)
+        for i in data:
+            Days_Present.append(float(i["Days Present"]))
+            Percentage.append(float(i["Marks In Percentage"]))
+
+        return {"x": Days_Present, "y": Percentage}
+
+
+def correlation(dataSource):
+    correlation_data = np.corrcoef(dataSource["x"], dataSource["y"])
+    print(correlation_data[0, 1])
+
+
+def setup():
+    data_path = "Student Marks vs Days Present.csv"
+    data_source = fetch_data_source(data_path)
+    correlation(data_source)
+
+setup()
